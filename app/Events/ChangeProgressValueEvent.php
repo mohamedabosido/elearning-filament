db26ac7a42b68d11ac09516ed84a77fddd49dd90
Mohamed Abosido <62311753+mohamedabosido@users.noreply.github.com>
2025-11-09 13:34:30 +0200

    Add broadcasting support with Laravel Echo and Reverb integration

---
+++ app/Events/ChangeProgressValueEvent.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ChangeProgressValueEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $value;
+    /**
+     * Create a new event instance.
+     */
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('progress'),
+        ];
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'progress.changed';
+    }
+
+    public function broadcastWith(): array
+    {
+        return ['value' => $this->value, 'timestamp' => now()->toDateTimeString()];
+    }
+}
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: "127.0.0.1",
+        port: 4011,
+        hmr: false,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
